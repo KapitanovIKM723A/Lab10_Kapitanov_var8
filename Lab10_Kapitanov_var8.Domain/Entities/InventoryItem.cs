4f4c72000c023cb3f6c4d4380926931019d1f9a2
Михайло Капітанов <Mykhailo.Kapitanov@infiz.khpi.edu.ua>
2025-05-24 19:50:34 +0300

    Added category.cs in entities Added InventoryController.cs Added autoMapper.cs

---
+++ Lab10_Kapitanov_var8.Domain/Entities/InventoryItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10_Kapitanov_var8.Domain.Entities
+{
+    internal class InventoryItem
+    {
+    }
+}
